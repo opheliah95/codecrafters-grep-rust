0b039b776546d0e8a2c557842c80be44e5c4cdde
opheliah95 <opheliah95@gmail.com>
2026-08-29 17:22:22 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,10 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    // TODO: Uncomment the code below to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    //TODO: Uncomment the code below to pass the first stage
+    if match_pattern(&input_line, &pattern) {
+        process::exit(0)
+    } else {
+        process::exit(1)
+    }
 }
