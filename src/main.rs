e000a874d82e829a7ceaf2f00070a8a510175d23
opheliah95 <opheliah95@gmail.com>
2026-08-30 21:31:58 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ use std::hash::Hash;
 use std::io;
 use std::process;
 
+fn check_all_true(vec: &Vec<bool>) -> bool {
+    if vec.len() == 0 {
+        return false;
+    }
+    vec.iter().all(|e| *e == true)
+}
+
 // currently handles \d \input -> 1 apple
 fn pattern_parser(input_line: &str, pattern: &str) -> bool {
     let input_parts: Vec<&str> = input_line.splitn(2, " ").collect();
@@ -19,39 +26,42 @@ fn pattern_parser(input_line: &str, pattern: &str) -> bool {
             println!("now spilt inputs are..{:?}", input_2_split);
             let re_spilt = input_line.split(" ").collect::<Vec<&str>>();
             for (key, val) in re_spilt.iter().enumerate() {
-                if match_pattern(val, pattern_1) {
+                println!("key {} val {} matching {}", key, val, pattern_1);
+                check_input_pattern(val, pattern_1, &mut res, "\\d");
+                println!("now res is {:?}", res);
+                if res.len() == 0 {
+                    return false;
+                }
+                if check_all_true(&res) {
                     // reach the end then false
+                    println!("check passed");
                     if key == re_spilt.len() - 1 {
                         return false;
                     }
                     // get next index and if it match then pass
                     let next = re_spilt[key + 1];
-                    if match_pattern(next, pattern_2) {
-                        return true;
-                    }
-                } 
+                    res = Vec::new();
+                    check_input_pattern(next, pattern_2, &mut res, "\\w");
+                    return check_all_true(&res);
+                }
+
+                res = Vec::new(); // re init bool vec
             }
 
             return false;
         }
 
-        let mut map: HashMap<&str, &str> =
-            HashMap::from([(input_1, pattern_1), (input_2, pattern_2)]);
-
-        if let Some(value) = check_input_pattern(input_1, pattern_1, &mut res, "\\d") {
-            return value;
-        }
-
-        if let Some(value) = check_input_pattern(input_2, pattern_2, &mut res, "\\w") {
-            return value;
-        }
+        // UPDATE RES VALUE 
+        res =Vec::new();
+        check_input_pattern(input_1, pattern_1, &mut res, "\\d");
+        check_input_pattern(input_2, pattern_2, &mut res, "\\w");
 
         match res.len() {
             0 => {
                 return false;
             }
             _ => {
-                return res.iter().all(|e| *e == true);
+                return check_all_true(&res);
             }
         }
     }
@@ -105,21 +115,24 @@ fn check_input_pattern(
     }
 
     let mut pt_count: Vec<_> = pattern.match_indices(input_ptn).map(|(i, _)| i).collect();
+    println!("pt_count {:?} and input {} and pattern {}", pt_count, input, pattern);
     if pt_count.len() == 1 {
         let matched = match_pattern(input, pattern);
+        println!("{input} matched one pattern");
         res.push(matched);
+        return Some(true);
     } else {
         //println!("more than one pattern {} {:?} {}", pattern, pt_count, input);
         let pt_count_len = pt_count.len();
         if pt_count_len == input.len() {
-            //println!("matching...{}", input);
+            println!("matching...{}", input);
             for i in [0..input.len()] {
                 let matched = match_pattern(&input[i], input_ptn);
                 res.push(matched);
                 return Some(true);
             }
         } else if pt_count_len == 0 {
-            //println!("current matching: ...{:?}", pt_count);
+            println!("current matching: ...{:?}", pt_count);
             res.push(false); //empty string error
             return Some(false);
         } else if pt_count_len != 0 && pt_count_len < input.len() {
@@ -129,14 +142,14 @@ fn check_input_pattern(
             let input_not_matched = &input[last_ptn_pos..];
             let matchable_input = &input[0..last_ptn_pos + 1];
 
-            // println!(
-            //     "DBG last pt: {last_ptn_pos}, final_ptn:  {final_ptn}, input_not_matched: {input_not_matched}"
-            // );
+            println!(
+                "DBG last pt: {last_ptn_pos}, final_ptn:  {final_ptn}, input_not_matched: {input_not_matched}"
+            );
             if final_ptn != input_not_matched {
                 res.push(false);
                 return Some(false);
             } else {
-                //println!("last pt: {last_ptn_pos}, final_ptn:  {final_ptn}, input_not_matched: {input_not_matched}")
+                println!("last pt: {last_ptn_pos}, final_ptn:  {final_ptn}, input_not_matched: {input_not_matched}");
                 for i in [0..matchable_input.len()] {
                     let matched = match_pattern(&matchable_input[i], input_ptn);
                     res.push(matched);
