81bd87189b8b02037c4298d048e4b2ef08e12040
opheliah95 <opheliah95@gmail.com>
2026-09-03 23:28:54 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -291,13 +291,13 @@ fn match_pattern(mut input_line: &str, mut pattern: &str) -> bool {
                 for val in ptn_spilt {
                     println!("eval if {input_line} contain {val}");
                     contain_alt.push(input_line.contains(val));
-                    println!("the vec is {:?} and result of sort is {} ", contain_alt);
+                    println!("the vec is {:?} ", contain_alt);
 
                 }   
 
                 return contain_alt.iter().any(|v| *v ==true);
             }
-            return false;
+           
         }
 
         // check wildcard
