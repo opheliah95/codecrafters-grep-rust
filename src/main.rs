efa363d566c4b86d221d22cfdfb7a4504173592b
opheliah95 <opheliah95@gmail.com>
2026-09-05 01:17:41 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -494,19 +494,18 @@ fn match_pattern(mut input_line: &str, mut pattern: &str) -> bool {
                                             );
 
                                             if val != after_zero_quant {
-                                                 return false;
-                                                // // if this reaches the end of line then this passed
-                                                // if idx == input_line.len() - 1 {
-                                                //     return true;
-                                                // }
-                                                // if val != pattern_char.clone().nth(idx + 1).unwrap()
-                                                // {
-                                                //     println!(
-                                                //         "{val} does not match ptn {}",
-                                                //         after_zero_quant
-                                                //     );
+                                                // if this reaches the end of line then this passed
+                                                if idx == input_line.len() - 1 {
+                                                    return true;
+                                                }
+                                                if val != pattern_char.clone().nth(idx + 1).unwrap()
+                                                {
+                                                    println!(
+                                                        "{val} does not match ptn {}",
+                                                        after_zero_quant
+                                                    );
                                                    
-                                                // }
+                                                }
                                             }
                                             println!(
                                                 "@@@MATCHED {val} matched ptn {}@@@",
