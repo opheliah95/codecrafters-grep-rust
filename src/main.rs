344c900d67eb90611421097da9f0e8597569c301
opheliah95 <opheliah95@gmail.com>
2026-09-06 01:34:57 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -758,7 +758,7 @@ fn main() {
     if split_input == 1 {
         println!("{input_line} is a single word ine input");
         if match_pattern(&input_line, &pattern) {
-            println!("single input pattern {input_line} passed");
+            println!("{input_line}");
             process::exit(0)
         } else {
             process::exit(1)
