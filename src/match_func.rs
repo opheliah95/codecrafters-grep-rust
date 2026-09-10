cd811e7eb4a507f51e0caa1077076d058ef202a8
opheliah95 <opheliah95@gmail.com>
2026-09-10 13:48:30 +0100

    codecrafters submit [skip ci]

--- src/match_func.rs
+++ src/match_func.rs
@@ -491,7 +491,7 @@ pub fn print_single_matching_line( input_line: &String, pattern: &mut String) ->
         // );
         let mut res: Vec<char> = Vec::new();
         if match_pattern(val, pattern) {
-            //println!("==={val} MATCHED {pattern}===");
+            println!("==={val} MATCHED {pattern}===");
             if pattern == "\\d" {
                 for v_char in val.chars().into_iter() {
                     //println!("{v_char} is a digit...");
