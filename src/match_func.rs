9c86de623134a83f14194289136e09e3a53cb176
opheliah95 <opheliah95@gmail.com>
2026-09-12 16:56:39 +0100

    codecrafters submit [skip ci]

--- src/match_func.rs
+++ src/match_func.rs
@@ -560,7 +560,7 @@ pub fn print_single_matching_line(input_line: &String, pattern: &mut String) -> 
     }
 
     for (idx, val) in input_slice.into_iter().enumerate() {
-        println!("{idx}: {val} vs {pattern}");
+        //println!("{idx}: {val} vs {pattern}");
         if let Some(repeat_matched) = examine_repeat(val, pattern) {
             //println!("repeat: {repeat_matched}");
             return repeat_matched.trim().to_string();
