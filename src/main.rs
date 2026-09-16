9e1a1dcbb6f32ae821f7c4cabf26ffed7a103917
opheliah95 <opheliah95@gmail.com>
2026-09-16 16:03:56 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -231,11 +231,7 @@ fn main() {
         if !color_always {
             println!("{}", input_line);
         } else {
-            if res.len() == input_has_space {
-                println!("{}", res.join(""));
-            } else {
-                println!("{input_line}");
-            }
+             println!("{}", res.join(""));
         }
     } else if res.len() > 0 && res.len() >= ptn_len_by_space {
         for r in res.chunks(ptn_len_by_space) {
