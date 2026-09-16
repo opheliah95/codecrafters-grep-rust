9cfcaf151a3c468e677b88a8e8d38ad20398c97a
opheliah95 <opheliah95@gmail.com>
2026-09-16 16:14:23 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -233,7 +233,7 @@ fn main() {
         } else {
              println!("{}", res.join(""));
         }
-    }  else if res.len() > 0 && env::args().nth(1).unwrap() == "-E" && !color_always && input_line.contains("\n") {
+    }  else if res.len() > 0 && env::args().nth(1).unwrap() == "-E" && !color_always && !input_line.contains("\n") {
         println!("{input_line}");
     }
     
