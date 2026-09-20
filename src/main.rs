0597030d5979a02a12cd713152b055c00e3b11a1
opheliah95 <opheliah95@gmail.com>
2026-09-20 17:38:45 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -145,7 +145,7 @@ fn handle_pattern_matching(
     match_found: &mut Vec<bool>,
     total_files:usize
 ) {
-    eprintln!("total files {total_files}");
+    eprintln!("at filename:={filename} Total files {total_files} and file_count = {file_count}");
     let suffix = if filename.is_empty() || (file_count <= 1 && total_files==file_count) {
         ""
     } else {
